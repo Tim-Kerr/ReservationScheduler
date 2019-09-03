9e7fc0083653ae877ea98fc08b7d5f7c79fa5bf1
Timmy <tkerr@riafinancial.com>
2019-09-03 14:27:46 -0600

    master: Removed test folder from tsc compilation includes

--- build/parseInput.js
+++ build/parseInput.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var moment = require("moment");
 // Consume input json file and parse date strings into Moment date objects
 // Moment.js wraps native JavaScript dates and exposes a date arithmetic api
-exports.default = (function (jsonFilepath) { return JSON.parse(fs.readFileSync(process.argv[2], 'utf8'), function (key, value) {
+exports.default = (function (jsonFilepath) { return JSON.parse(fs.readFileSync(jsonFilepath, 'utf8'), function (key, value) {
     if (key === 'startDate')
         return moment(value);
     // End dates are inclusive. Add a day to make the math work.
